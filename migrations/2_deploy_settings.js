dadd4eb65f0940cb0c24113d9ee4d8bfa0666711
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2018-01-12 01:06:24 -0500

    deploy with arguments from the constants file

--- migrations/2_deploy_settings.js
+++ migrations/2_deploy_settings.js
@@ -1,5 +1,11 @@
 const GameSettings = artifacts.require('GameSettings');
+const constants = require('./util/constants');
 
 module.exports = function (deployer) {
-  deployer.deploy(GameSettings);
+  deployer.deploy(
+    GameSettings,
+    constants.minDeposit, constants.maxDeposit,
+    constants.ticketTtlSeconds,
+    constants.joinFeePercentage, constants.minFee, constants.maxFee
+  );
 };
--- migrations/util/constants.js
+++ migrations/util/constants.js
@@ -2,7 +2,8 @@ module.exports = {
   minDeposit: Math.pow(10, 17),
   maxDeposit: Math.pow(10, 18),
   ticketTtlSeconds: 300,
-  joinFeePercentage: 1,
-  minFee: 0.01 * Math.pow(10,18),
+  joinFeePercentage: 5,
+  // 1-10 dollar min/max
+  minFee: 0.001 * Math.pow(10,18),
   maxFee: 0.01 * Math.pow(10,18)
 };
